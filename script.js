4c3de68e41f167b18bd7288a82a40cf23c7d07ab
Geraldine <hello@geraldiner.com>
2022-01-25 16:30:55 -0500

    add: initial js functionality is working

---
+++ script.js
@@ -0,0 +1,282 @@
+/**
+ * Lists of recipes as objects for accessing the data for each easier and avoid hard-coding
+ */
+// List of NomNom dog food recipes as objects
+const NOMNOM_RECIPES_DOG = [
+	{
+		"name": "Beef Mash",
+		"protein": 77,
+		"fat": 40,
+		"carb": 111,
+		"imgUrl": "https://www.nomnomnow.com/images/recipes/bowl_beef_square.png",
+	},
+	{
+		"name": "Chicken Cuisine",
+		"protein": 80,
+		"fat": 56,
+		"carb": 71,
+		"imgUrl": "https://www.nomnomnow.com/images/recipes/bowl_chicken_square.png",
+	},
+	{
+		"name": "Turkey Fare",
+		"protein": 78,
+		"fat": 41,
+		"carb": 109,
+		"imgUrl": "https://www.nomnomnow.com/images/recipes/bowl_turkey_square.png",
+	},
+	{
+		"name": "Pork Potluck",
+		"protein": 76,
+		"fat": 48,
+		"carb": 93,
+		"imgUrl": "https://www.nomnomnow.com/images/recipes/bowl_pork_square.png",
+	},
+];
+
+// List of NomNom cat food recipes as objects
+const NOMNOM_RECIPES_CAT = [
+	{
+		"name": "Chicken Cuisine",
+		"protein": 163,
+		"fat": 34,
+		"carb": 41,
+		"imgUrl": "https://www.nomnomnow.com/images/recipes/bowl_cat_chicken_square.png",
+	},
+];
+
+// List of dog food brands as objects
+const PET_FOOD_BRANDS_DOG = [
+	{
+		"name": "Blue Buffalo ® Wilderness ™ Chicken",
+		"protein": 99,
+		"fat": 44,
+		"carb": 79,
+		"proteinPercent": 34,
+		"fatPercent": 15,
+		"cal": 3599,
+		"imgUrl": "https://www.nomnomnow.com/images/brands/blue_buffalo_chicken.jpg",
+	},
+	{
+		"name": "Hill's ® Chicken & Brown Rice",
+		"protein": 54,
+		"fat": 40,
+		"carb": 135,
+		"proteinPercent": 19,
+		"fatPercent": 14,
+		"cal": 3784,
+		"imgUrl": "https://www.nomnomnow.com/images/brands/hill_chicken.jpg",
+	},
+	{
+		"name": "Holistic Select ® Chicken & Brown Rice",
+		"protein": 68,
+		"fat": 41,
+		"carb": 79,
+		"proteinPercent": 25,
+		"fatPercent": 15,
+		"cal": 3916,
+		"imgUrl": "https://www.nomnomnow.com/images/brands/holistic_select_chicken_rice.jpg",
+	},
+];
+
+// List of cat food brands as objects
+const PET_FOOD_BRANDS_CAT = [
+	{
+		"name": "Blue Buffalo ® Wilderness ™ Chicken",
+		"protein": 108,
+		"fat": 50,
+		"carb": 57,
+		"proteinPercent": 40,
+		"fatPercent": 18,
+		"cal": 3832,
+		"imgUrl": "https://www.nomnomnow.com/images/brands/blue_buffalo_cat_chicken.jpg",
+	},
+	{
+		"name": "Iams ® Chicken",
+		"protein": 90,
+		"fat": 43,
+		"carb": 92,
+		"proteinPercent": 32,
+		"fatPercent": 15,
+		"cal": 3740,
+		"imgUrl": "https://www.nomnomnow.com/images/brands/iams_cat_chicken.jpg",
+	},
+	{
+		"name": "Natural Balance ® Green Pea & Chicken",
+		"protein": 88,
+		"fat": 36,
+		"carb": 111,
+		"proteinPercent": 30,
+		"fatPercent": 12,
+		"cal": 3600,
+		"imgUrl": "https://www.nomnomnow.com/images/brands/natural_balance_cat_chicken.jpg",
+	},
+];
+
+/**
+ * Global variables to be accessed throughout the code, especially in EventListeners
+ */
+// type of pet food being compared, either dog or cat
+let petType;
+// selected pet food from dropdown, intially undefined
+let petFoodBrand;
+// selected NomNom recipe, initalized to whatever's first in the list based on petType
+let nomNomRecipe;
+
+/**
+ * DOM elements and EventListeners
+ *
+ */
+// Radio buttons for choosing between dog and cat
+const petTypeRadioBtns = document.querySelectorAll('input[name="petType"]');
+// EventListener for each radio button to set the current petType to the selected radio button. Then update the selection for pet food brands and NomNom recipes based on the new petType.
+petTypeRadioBtns.forEach(r => {
+	r.addEventListener("click", e => {
+		e.preventDefault();
+		petType = e.target.value;
+		updatePetFoodSelection();
+		updateNomNomSelection();
+	});
+});
+
+// Dropdown menu for selecting the pet food brand
+const petFoodSelect = document.querySelector("#pet-food-brand-select");
+// EventListener to change the current petFoodBrand when the user makes a new selection. Then update the results section.
+petFoodSelect.onchange = e => {
+	e.preventDefault();
+	petFoodBrand = petType === "dog" ? PET_FOOD_BRANDS_DOG[e.target.value] : PET_FOOD_BRANDS_CAT[e.target.value];
+	updatePetFoodResults();
+	updateNomNomResults();
+};
+
+// Dropdown menu for selecting the NomNom recipe
+const nomNomRecipeSelect = document.querySelector("#nomnom-recipe-select");
+// EventListener to change the current nomNomRecipe when the user makes a new selection. Then update the results section.
+nomNomRecipeSelect.onchange = e => {
+	e.preventDefault();
+	nomNomRecipe = petType === "dog" ? NOMNOM_RECIPES_DOG[e.target.value] : NOMNOM_RECIPES_CAT[e.target.value];
+	updateNomNomResults(e);
+};
+
+// Once the window loads, initialize petType to dog (initially checked). Then update the dropdown menus for dog food/recipes.
+window.onload = () => {
+	petType = "dog";
+	updatePetFoodSelection();
+	updateNomNomSelection();
+};
+
+// Update the pet food dropdown menu based on the current petType
+function updatePetFoodSelection() {
+	petFoodSelect.innerHTML = `<option value="">Select Pet Food Brand</option>`;
+	if (petType === "dog") {
+		for (let i = 0; i < PET_FOOD_BRANDS_DOG.length; i++) {
+			let option = document.createElement("option");
+			option.value = i;
+			option.innerText = PET_FOOD_BRANDS_DOG[i].name;
+			petFoodSelect.appendChild(option);
+		}
+	} else {
+		for (let i = 0; i < PET_FOOD_BRANDS_CAT.length; i++) {
+			let option = document.createElement("option");
+			option.value = i;
+			option.innerText = PET_FOOD_BRANDS_CAT[i].name;
+			petFoodSelect.appendChild(option);
+		}
+	}
+}
+
+// Update the NomNom dropdown menu based on the current petType
+function updateNomNomSelection() {
+	nomNomRecipeSelect.innerHTML = "";
+	if (petType === "dog") {
+		nomNomRecipe = NOMNOM_RECIPES_DOG[0];
+		for (let i = 0; i < NOMNOM_RECIPES_DOG.length; i++) {
+			let option = document.createElement("option");
+			option.value = i;
+			option.innerText = NOMNOM_RECIPES_DOG[i].name;
+			nomNomRecipeSelect.appendChild(option);
+		}
+	} else {
+		nomNomRecipe = NOMNOM_RECIPES_CAT[0];
+		for (let i = 0; i < NOMNOM_RECIPES_CAT.length; i++) {
+			let option = document.createElement("option");
+			option.value = i;
+			option.innerText = NOMNOM_RECIPES_CAT[i].name;
+			nomNomRecipeSelect.appendChild(option);
+		}
+	}
+}
+
+// Update the pet food results based on the current petFoodBrand
+function updatePetFoodResults() {
+	const resultsP = document.querySelector("#results>p");
+	resultsP.innerText = "";
+
+	resultsP.innerText = `${petFoodBrand.name} contains ${petFoodBrand.proteinPercent}% min protein, ${petFoodBrand.fatPercent}% min fat and has a calorie density of ${petFoodBrand.cal}kcal/kg.`;
+
+	const petFoodResults = document.querySelector("#pet-food-results");
+	petFoodResults.innerHTML = "";
+	const imgPetFood = document.createElement("img");
+	imgPetFood.src = petFoodBrand.imgUrl;
+	imgPetFood.alt = `${petFoodBrand.name} logo`;
+
+	const h4ProteinPetFood = document.createElement("h4");
+	const spanProteinPetFood = document.createElement("span");
+
+	h4ProteinPetFood.innerText = "Protein";
+	spanProteinPetFood.innerText = petFoodBrand.protein;
+
+	const h4FatPetFood = document.createElement("h4");
+	const spanFatPetFood = document.createElement("span");
+
+	h4FatPetFood.innerText = "Fat";
+	spanFatPetFood.innerText = petFoodBrand.fat;
+
+	const h4CarbPetFood = document.createElement("h4");
+	const spanCarbPetFood = document.createElement("span");
+
+	h4CarbPetFood.innerText = "Carbohydrate";
+	spanCarbPetFood.innerText = petFoodBrand.carb;
+
+	petFoodResults.appendChild(imgPetFood);
+	petFoodResults.appendChild(h4ProteinPetFood);
+	petFoodResults.appendChild(spanProteinPetFood);
+	petFoodResults.appendChild(h4FatPetFood);
+	petFoodResults.appendChild(spanFatPetFood);
+	petFoodResults.appendChild(h4CarbPetFood);
+	petFoodResults.appendChild(spanCarbPetFood);
+}
+
+// Update the NomNom results based on the current nomNomRecipe
+function updateNomNomResults(e) {
+	const nomNomResults = document.querySelector("#nomnom-results");
+	nomNomResults.innerHTML = "";
+	const imgNomNom = document.createElement("img");
+	imgNomNom.src = nomNomRecipe.imgUrl;
+	imgNomNom.alt = `${nomNomRecipe.name} logo`;
+
+	const h4ProteinNomNom = document.createElement("h4");
+	const spanProteinNomNom = document.createElement("span");
+
+	h4ProteinNomNom.innerText = "Protein";
+	spanProteinNomNom.innerText = nomNomRecipe.protein;
+
+	const h4FatNomNom = document.createElement("h4");
+	const spanFatNomNom = document.createElement("span");
+
+	h4FatNomNom.innerText = "Fat";
+	spanFatNomNom.innerText = nomNomRecipe.fat;
+
+	const h4CarbNomNom = document.createElement("h4");
+	const spanCarbNomNom = document.createElement("span");
+
+	h4CarbNomNom.innerText = "Carbohydrate";
+	spanCarbNomNom.innerText = nomNomRecipe.carb;
+
+	nomNomResults.appendChild(imgNomNom);
+	nomNomResults.appendChild(h4ProteinNomNom);
+	nomNomResults.appendChild(spanProteinNomNom);
+	nomNomResults.appendChild(h4FatNomNom);
+	nomNomResults.appendChild(spanFatNomNom);
+	nomNomResults.appendChild(h4CarbNomNom);
+	nomNomResults.appendChild(spanCarbNomNom);
+}
